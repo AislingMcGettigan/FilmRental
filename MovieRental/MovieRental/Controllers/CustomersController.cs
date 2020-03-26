fb00659281abf6ede8aa48506ef92da055789985
aisling mcgettigan <aislingmcgettigan93@hotmail.com>
2020-03-25 17:27:28 -0700

    Movies and Customers

---
+++ MovieRental/MovieRental/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+using MovieRental.ViewModels;
+
+namespace MovieRental.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer(){Name ="John Smith" , Id=1},
+                new Customer(){Name = "Mary Williams", Id=2}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id == 1)
+            {
+                return Content("John Smith");
+            }
+
+            if (id == 2)
+            {
+                return Content("Mary Williams");
+            }
+
+            return Content("Unknown");
+        }
+    }
+}
---
+++ MovieRental/MovieRental/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+using MovieRental.ViewModels;
+
+namespace MovieRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new List<Movie>(){
+                new Movie() { Name = "Shrek" },
+                new Movie() { Name = "Wall-e" }
+            };
+
+            var customers = new List<Customer>()
+            {
+                new Customer(){Name = "Customer1"},
+                new Customer(){Name = "Customer2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+    }
+
+}
