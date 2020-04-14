1f4cbc2063f4033b1e717263eecb8411ea64c723
aisling mcgettigan <aislingmcgettigan93@hotmail.com>
2020-04-13 20:45:10 -0700

    rentals

---
+++ MovieRental/MovieRental/Controllers/API/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using MovieRental.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieRental.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDTO newRentalDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
